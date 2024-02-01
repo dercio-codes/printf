f913e9de26d4579e020f32dcbd64f6ab2b2e3e5c
dercio-codes <12dercimaduna@gmail.com>
2024-02-01 02:47:53 -0800

    fixed betty

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...)
 {
 	int i = 0, printed = 0, printed_chars = 0;
-	int /*flags, width, precision, size,*/ buff_ind = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -26,16 +26,15 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			//flags = get_flags(format, &i);
-			//width = get_width(format, &i, list);
-			//precision = get_precision(format, &i, list);
-			//size = get_size(format, &i);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer);
 			if (printed == -1)
